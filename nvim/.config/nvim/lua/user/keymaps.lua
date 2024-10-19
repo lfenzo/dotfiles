52c92f9e82ca0ea7fb3ff9810cf3bbdb1f2046fa
lfenzo <lfenzo@protonmail.com>
2024-10-19 10:27:19 -0300

    nvim: added neovim config files

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,29 @@
+-- Shorten function name
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+
+-- Neotree
+keymap("n", "<Leader>e", ":Neotree toggle<cr>", opts)
+
+-- Telescope
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, {})
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, {})
+vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, {})
+
+
+-- Lazygit
+vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", opts)
