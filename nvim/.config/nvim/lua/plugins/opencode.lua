5ffc0a2241d17376e60db3d900864bb0cf805461
lfenzo <lfenzo@protonmail.com>
2026-06-15 08:51:29 -0300

    nvim: refactor opencode plugin config

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -1,35 +1,39 @@
+local opencode = require("opencode")
+
+vim.o.autoread = true
+
 require("snacks").setup({
     input = {},
     picker = {
         actions = {
             opencode_send = function(...)
-                return require("opencode").snacks_picker_send(...)
+                return opencode.snacks_picker_send(...)
             end,
         },
         win = {
             input = {
                 keys = {
-                    ["<a-a>"] = {
-                        "opencode_send",
-                        mode = { "n", "i" },
-                    },
+                    ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
                 },
             },
         },
     },
 })
 
+local map = vim.keymap.set
 
-vim.o.autoread = true -- Required for `opts.events.reload`
+map({ "n", "x" }, "<leader>o", function()
+    opencode.ask("@this: ", { submit = true })
+end, { desc = "Ask opencode…" })
 
-vim.keymap.set({ "n", "x" }, "<leader>o", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end, { desc = "Select opencode…" })
--- vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
-vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end, {
-  desc = "Add range to opencode",
-  expr = true,
-})
-vim.keymap.set("n", "goo", function() return require("opencode").operator("@this ") .. "_" end, {
-  desc = "Add line to opencode",
-  expr = true,
-})
+map({ "n", "x" }, "<C-x>", function()
+    opencode.select()
+end, { desc = "Select opencode…" })
+
+map({ "n", "x" }, "go", function()
+    return opencode.operator("@this ")
+end, { desc = "Add range to opencode", expr = true })
+
+map("n", "goo", function()
+    return opencode.operator("@this ") .. "_"
+end, { desc = "Add line to opencode", expr = true })
